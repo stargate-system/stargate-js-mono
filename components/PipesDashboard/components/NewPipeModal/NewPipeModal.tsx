206b50fdb8225ab2b78da440cbdbd26b3734ffce
MichalMarchel84 <michal.marchel84@gmail.com>
2023-12-23 15:18:14 +0100

    Logic for Create Pipe modal

--- components/PipesDashboard/components/NewPipeModal/NewPipeModal.tsx
+++ components/PipesDashboard/components/NewPipeModal/NewPipeModal.tsx
@@ -5,6 +5,7 @@ import styles from './NewPipeModal.module.css';
 import ValueSelect from "./components/ValueSelect/ValueSelect";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faArrowDown} from "@fortawesome/free-solid-svg-icons";
+import {AddressMapper} from "gate-core";
 
 interface NewPipeModalProps {
     systemModel: SystemModel
@@ -22,26 +23,61 @@ const NewPipeModal = (props: NewPipeModalProps) => {
     const [targetDevice, setTargetDevice] = useState<DeviceModel | undefined>();
     const [targetValue, setTargetValue] = useState<GateValueModel | undefined>();
 
-    const getSourceDevices = (): DeviceModel[] => {
-        return systemModel.devices.values;
+    const getDevices = (excluded?: DeviceModel): DeviceModel[] => {
+        let filteredDevices = systemModel.devices.values;
+        if (excluded) {
+            filteredDevices = filteredDevices.filter((device) => excluded.id !== device.id);
+        }
+        return filteredDevices;
     }
 
-    const getSourceValues = () => {
+    const getAvailableValues = (excludedDevice?: DeviceModel) => {
         const availableValues: GateValueModel[] = [];
         systemModel.devices.values.forEach((device) => {
-            device.gateValues.values.forEach((value) => {
-                availableValues.push(value);
-            })
+            if (!excludedDevice || excludedDevice.id !== device.id) {
+                device.gateValues.values.forEach((value) => {
+                    availableValues.push(value);
+                });
+            }
         });
         return availableValues;
     }
 
+    const getSourceValues = () => {
+        let excludedDevice: DeviceModel | undefined = sourceDevice;
+        if (sourceValue) {
+            excludedDevice = sourceDevices.find((device) => AddressMapper.extractTargetId(sourceValue.id)[0] === device.id);
+        }
+        return getAvailableValues(excludedDevice);
+    }
+
+    const getTargetValues = () => {
+        let excludedDevice: DeviceModel | undefined = targetDevice;
+        if (targetValue) {
+            excludedDevice = targetDevices.find((device) => AddressMapper.extractTargetId(targetValue.id)[0] === device.id);
+        }
+        return getAvailableValues(excludedDevice);
+    }
+
     useEffect(() => {
-        setSourceDevices(getSourceDevices());
+        setSourceDevices(getDevices(targetDevice));
         setSourceValues(getSourceValues());
-        setTargetDevices(getSourceDevices());
-        setTargetValues(getSourceValues());
-    }, []);
+    }, [targetDevice]);
+
+    useEffect(() => {
+        setTargetDevices(getDevices(sourceDevice));
+        setTargetValues(getTargetValues());
+    }, [sourceDevice]);
+
+    useEffect(() => {
+        setConfirmDisabled(!(sourceValue && targetValue));
+        setTargetValues(getTargetValues());
+    }, [sourceValue]);
+
+    useEffect(() => {
+        setConfirmDisabled(!(sourceValue && targetValue));
+        setSourceValues(getSourceValues());
+    }, [targetValue]);
 
     return (
         <StandardModal
@@ -49,12 +85,14 @@ const NewPipeModal = (props: NewPipeModalProps) => {
             approveDisabled={confirmDisabled}
             approveLabel={'Create'}
         >
+            Create pipe
             <div className={styles.newPipeModalBodyContainer}>
                 <ValueSelect
                     devices={sourceDevices}
                     values={sourceValues}
                     setSelectedDevice={setSourceDevice}
                     setSelectedValue={setSourceValue}
+                    typeFilter={targetValue?.gateValue.type}
                 />
                 <FontAwesomeIcon className={styles.arrowIcon} icon={faArrowDown}/>
                 <ValueSelect
@@ -62,6 +100,8 @@ const NewPipeModal = (props: NewPipeModalProps) => {
                     values={targetValues}
                     setSelectedDevice={setTargetDevice}
                     setSelectedValue={setTargetValue}
+                    typeFilter={sourceValue?.gateValue.type}
+                    excludeOutput={true}
                 />
             </div>
         </StandardModal>
--- components/PipesDashboard/components/NewPipeModal/components/ValueSelect/ValueSelect.tsx
+++ components/PipesDashboard/components/NewPipeModal/components/ValueSelect/ValueSelect.tsx
@@ -1,13 +1,16 @@
-import React, {Dispatch, SetStateAction, useEffect, useMemo, useState} from "react";
+import React, {Dispatch, SetStateAction, useMemo, useState} from "react";
 import styles from './ValueSelect.module.css';
 import {DeviceModel, GateValueModel} from "gate-viewmodel";
 import Select from 'react-select';
+import {AddressMapper, Directions, ValueTypes} from "gate-core";
 
 interface ValueSelectProps {
     devices: DeviceModel[],
     values: GateValueModel[],
     setSelectedDevice: Dispatch<SetStateAction<DeviceModel | undefined>>,
-    setSelectedValue: Dispatch<SetStateAction<GateValueModel | undefined>>
+    setSelectedValue: Dispatch<SetStateAction<GateValueModel | undefined>>,
+    typeFilter?: string,
+    excludeOutput?: boolean
 }
 
 interface SelectedOption {
@@ -20,11 +23,13 @@ const ValueSelect = (props: ValueSelectProps) => {
         devices,
         values,
         setSelectedDevice,
-        setSelectedValue
+        setSelectedValue,
+        typeFilter,
+        excludeOutput = false
     } = props;
 
-    const [deviceOption, setDeviceOption] = useState<SelectedOption | undefined>();
-    const [valueOption, setValueOption] = useState<SelectedOption | undefined>();
+    const [deviceOption, setDeviceOption] = useState<SelectedOption | null>(null);
+    const [valueOption, setValueOption] = useState<SelectedOption | null>(null);
 
     const getName = (name?: string) => {
         if (name && name.length) {
@@ -42,43 +47,74 @@ const ValueSelect = (props: ValueSelectProps) => {
         )
     }
 
+    const getCompatibleTypes = (): Array<string | undefined> => {
+        if (typeFilter === ValueTypes.float || typeFilter === ValueTypes.integer) {
+            return [ValueTypes.float, ValueTypes.integer];
+        }
+        return [typeFilter];
+    }
+
     const deviceOptions: SelectedOption[] = useMemo(() => {
         const options: SelectedOption[] = devices.map((device) => {
             return {value: device, label: getName(device.name.value)}
         })
-        options.unshift({value: undefined, label: '-- select device --'});
+        options.unshift({value: undefined, label: '-- none --'});
         return options;
     }, [devices]);
 
+    const filterByType = (values: GateValueModel[]) => {
+        if (typeFilter) {
+            const compatibleTypes = getCompatibleTypes();
+            return values.filter((value) => {
+                return !!compatibleTypes.find((type) => type === value.gateValue.type);
+            });
+        }
+        return values;
+    }
+
+    const emptyOption = useMemo<SelectedOption>(() => {
+        return {value: undefined, label: '-- none --'};
+    }, []);
+
     const valueOptions: SelectedOption[] = useMemo(() => {
-        const options: SelectedOption[] = values.map((value) => {
-            return {value: value, label: getTypeLabel(value)}
-        });
-        options.unshift({value: undefined, label: '-- select value --'});
+        let options: SelectedOption[];
+        if (deviceOption) {
+            const availableValues = filterByType((deviceOption.value as DeviceModel).gateValues.values);
+            options = availableValues.map((value) => {
+                return {value: value, label: getTypeLabel(value)}
+            });
+        } else {
+            options = filterByType(values).map((value) => {
+                return {value: value, label: getTypeLabel(value)}
+            });
+        }
+        if (excludeOutput) {
+            options = options.filter(option => (option.value as GateValueModel).gateValue.direction !== Directions.output)
+        }
+        options.unshift(emptyOption);
         return options;
-    }, [values]);
+    }, [values, deviceOption, typeFilter]);
 
     const onDeviceChange = (selected: SelectedOption) => {
-        setSelectedDevice(selected.value as DeviceModel);
-        setDeviceOption(selected);
+        const device = selected.value ? selected.value as DeviceModel : undefined;
+        setSelectedDevice(device);
+        setDeviceOption(selected.value ? selected : null);
+        if (valueOption && device && !device.gateValues.values.find((value) => value.id === valueOption.value?.id)) {
+            onValueChange(emptyOption);
+        }
     }
 
     const onValueChange = (selected: SelectedOption) => {
-        setSelectedValue(selected.value as GateValueModel);
-        setValueOption(selected);
+        if (selected.value) {
+            const parentId = AddressMapper.extractTargetId(selected.value.id)[0];
+            const deviceOption = deviceOptions.find((device) => device.value?.id === parentId);
+            if (deviceOption) {
+                onDeviceChange(deviceOption);
+            }
+        }
+        setSelectedValue(selected.value ? selected.value as GateValueModel : undefined);
+        setValueOption(selected.value ? selected : null);
     }
-
-    useEffect(() => {
-        if (deviceOption === undefined) {
-            setDeviceOption(deviceOptions[0]);
-        }
-    }, [devices]);
-
-    useEffect(() => {
-        if (valueOption === undefined) {
-            setValueOption(valueOptions[0]);
-        }
-    }, [devices]);
 
     return (
         <div className={styles.valueSelectContainer}>
@@ -87,12 +123,14 @@ const ValueSelect = (props: ValueSelectProps) => {
                 options={deviceOptions}
                 value={deviceOption}
                 onChange={(selected) => onDeviceChange(selected as SelectedOption)}
+                placeholder={'Filter by device...'}
             />
             <Select
                 className={styles.selectInput}
                 options={valueOptions}
                 value={valueOption}
                 onChange={(selected) => onValueChange(selected as SelectedOption)}
+                placeholder={'Select value...'}
             />
         </div>
     )
