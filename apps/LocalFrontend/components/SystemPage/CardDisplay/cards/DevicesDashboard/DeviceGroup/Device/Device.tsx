b6473b51935579dd7b8e30239304136b2be7cb46
MichalMarchel84 <michal.marchel84@gmail.com>
2024-03-12 12:03:06 +0100

    Added simplified method of customizing device view

--- apps/LocalFrontend/components/SystemPage/CardDisplay/cards/DevicesDashboard/DeviceGroup/Device/Device.tsx
+++ apps/LocalFrontend/components/SystemPage/CardDisplay/cards/DevicesDashboard/DeviceGroup/Device/Device.tsx
@@ -1,12 +1,23 @@
-import {useCallback, useEffect, useMemo, useState} from "react";
+import React, {ReactNode, useCallback, useEffect, useMemo, useState} from "react";
 import DeviceHeader from "./components/DeviceHeader/DeviceHeader";
 import styles from './Device.module.css';
-import GateValueWrapper from "@/components/SystemPage/CardDisplay/cards/DevicesDashboard/DeviceGroup/Device/GateValue/GateValueWrapper";
-import {DeviceModel, DeviceState} from "gate-viewmodel";
+import GateValueWrapper
+    from "@/components/SystemPage/CardDisplay/cards/DevicesDashboard/DeviceGroup/Device/GateValue/GateValueWrapper";
+import {DeviceModel, DeviceState, GateValueModel} from "gate-viewmodel";
 import useModelValue from "@/components/ReactGateViewModel/hooks/useModelValue";
 import useModelMap from "@/components/ReactGateViewModel/hooks/useModelMap";
 import {ValueVisibility} from "gate-core";
 import {Categories, localStorageHelper} from "@/helper/localStorageHelper";
+
+const customMatchers: CustomDeviceMatcher[] = [
+    // put device values customizations here
+]
+
+export interface CustomDeviceMatcher {
+    isCustom: (deviceModel: DeviceModel) => boolean,
+    filterValues: (values: GateValueModel[]) => GateValueModel[],
+    getInstance: (key: string, deviceModel: DeviceModel) => ReactNode
+}
 
 interface DeviceProps {
     deviceModel: DeviceModel
@@ -34,15 +45,26 @@ const Device = (props: DeviceProps) => {
     }, [isActive]);
 
     const generateValues = useCallback(() => {
-        return values
-            .filter((value) => {
+        let customFilteredValues = [...values];
+        const customElementGetters: Array<(key: string, deviceModel: DeviceModel) => ReactNode> = [];
+        customMatchers.forEach((matcher) => {
+            if (matcher.isCustom(deviceModel)) {
+                customFilteredValues = matcher.filterValues(customFilteredValues);
+                customElementGetters.push(matcher.getInstance);
+            }
+        });
+
+        const elements: ReactNode[] = customElementGetters.map((getter, index) => getter(index.toString(), deviceModel));
+        customFilteredValues.filter((value) => {
                 const isSettings = value.gateValue.visibility === ValueVisibility.settings.toString();
                 const isHidden = value.gateValue.visibility === ValueVisibility.hidden.toString();
                 return !(isSettings || isHidden);
             })
-            .map((valueModel) => {
-                return <GateValueWrapper key={valueModel.id + Date.now()} valueModel={valueModel}/>
+            .forEach((valueModel) => {
+                elements.push(<GateValueWrapper key={valueModel.id + Date.now()} valueModel={valueModel}/>);
             });
+
+        return elements
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
