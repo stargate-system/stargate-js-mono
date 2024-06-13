d53ae7544b244f582e8bcf3ff56d204a8bbde4e2
MichalMarchel84 <michal.marchel84@gmail.com>
2024-06-13 10:30:01 +0200

    Added "Component in development" to Account button

--- apps/UserInterface/components/SystemPage/CardDisplay/components/AccountOptions/AccountOptions.tsx
+++ apps/UserInterface/components/SystemPage/CardDisplay/components/AccountOptions/AccountOptions.tsx
@@ -2,6 +2,8 @@ import styles from './AccountOptions.module.css';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faUser} from "@fortawesome/free-regular-svg-icons";
 import HeaderButton from "@/components/SystemPage/CardDisplay/components/HeaderButton/HeaderButton";
+import {useEffect, useRef, useState} from "react";
+import useClickOutsideDetector from "@/helper/useClickOutsideDetector";
 
 interface AccountOptionsProps {
     className?: string
@@ -9,12 +11,30 @@ interface AccountOptionsProps {
 
 const AccountOptions = (props: AccountOptionsProps) => {
     const {className} = props;
+    const [menuOpen, setMenuOpen] = useState(false);
+    const accountOptionsRef = useRef(null);
+    useClickOutsideDetector(accountOptionsRef, () => setMenuOpen(false));
+    const menuOpenRef = useRef(false);
+
+    useEffect(() => {
+        menuOpenRef.current = menuOpen;
+    }, [menuOpen]);
 
     return (
-        <div className={`${styles.accountOptionsContainer} ${className ?? ''}`}>
-            <HeaderButton onClick={() => {}}>
+        <div ref={accountOptionsRef} className={`${className ?? ''}`}>
+            <HeaderButton onClick={() => setMenuOpen(!menuOpenRef.current)}>
                 <FontAwesomeIcon icon={faUser} size="xl"/>
             </HeaderButton>
+            {menuOpen &&
+                <div className={styles.accountOptionsContainer}>
+                    <div className={`${styles.header} ${styles.highlight}`}>
+                        Component in development
+                    </div>
+                    <div className={styles.content}>
+                        Expected release: <span className={styles.highlight}>end of 2024</span>
+                    </div>
+                </div>
+            }
         </div>
     );
 }
